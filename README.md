5717ba7bff38a264dd441d82d7b6521225c7ed77
ROSHAN RAJ <roshan.raj2@microchip.com>
2023-04-28 20:52:34 +0530

    README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PIC32CXBZ2_WBZ45x_Sub-GHz_MICRF112-114_MICRF220-219A
+# PIC32CXBZ2_WBZ45x_Sub-GHz_MICRF112/114_MICRF220/219A
 
 <img src="docs/IoT-Made-Easy-Logo.png" width=100>
 
@@ -26,9 +26,12 @@ Checkout the <a href="https://microchipsupport.force.com/s/" target="_blank">Tec
 
 ## 1. Introduction<a name="step1">
 
-This application demonstrates the use of an MICRF112/114 as transmitter and MICRF220/219A as receiver using ASK Modulation interfaced with WBZ451 Curiosity Board to control the RGB LED of WBZ451 Curiosity board on receiver part. This LED can be controlled by Microchip Bluetooth Data ([(MBD)](https://play.google.com/store/apps/details?id=com.microchip.bluetooth.data&hl=en_IN&gl=US) application through Bluetooth Low Energy (BLE) using [BLE Sensor application](https://github.com/Microchip-MPLAB-Harmony/wireless_apps_pic32cxbz2_wbz45/tree/master/apps/ble/advanced_applications/ble_sensor).The temperature data is also periodically reported to the mobile phone through BLE.
+This application demonstrates the use of an MICRF112 as transmitter and MICRF220/219A as receiver using ASK Modulation interfaced with the WBZ451 Curiosity board to control the RGB LED of WBZ451 Curiosity board on receiver part. This LED can be controlled by Microchip Bluetooth Data [(MBD)](https://play.google.com/store/apps/details?id=com.microchip.bluetooth.data&hl=en_IN&gl=US) application through Bluetooth Low Energy (BLE) using [BLE Sensor application](https://github.com/Microchip-MPLAB-Harmony/wireless_apps_pic32cxbz2_wbz45/tree/master/apps/ble/advanced_applications/ble_sensor).
 
 ![](docs/Picture1.png)
+
+| Note: For more details on the working of BLE sensor application, refer to the link [here](https://github.com/Microchip-MPLAB-Harmony/wireless_apps_pic32cxbz2_wbz45/tree/master/apps/ble/advanced_applications/ble_sensor) !! |
+| --- |
 
 The [BLE Sensor application](https://github.com/Microchip-MPLAB-Harmony/wireless_apps_pic32cxbz2_wbz45/tree/master/apps/ble/advanced_applications/ble_sensor) is implemented on both transmitter part and receiver part to control the RGB LED. The RGB values are received from the (MBD) application using BLE Sensor application and then it is transmitted through MICRF112/114. The MICRF220/219A receives the RGB values and updates the RGB LED using [BLE Sensor application](https://github.com/Microchip-MPLAB-Harmony/wireless_apps_pic32cxbz2_wbz45/tree/master/apps/ble/advanced_applications/ble_sensor).
 
@@ -38,7 +41,7 @@ The above video shows the working of MICRF112(433MHz) and MICRF219A(433MHz).
 
 ## 2. Contents<a name="step2">
 
-1. [WBZ451_MICRF112_114](https://github.com/MicrochipTech/PIC32CXBZ2_WBZ45x_Sub-GHz_MICRF112_114_MICRF220_219A_BLE_SENSOR/tree/main/WBZ451_MICRF112_114)
+1. [WBZ451_MICRF112/114](https://github.com/MicrochipTech/PIC32CXBZ2_WBZ45x_Sub-GHz_MICRF112_114_MICRF220_219A_BLE_SENSOR/tree/main/WBZ451_MICRF112_114)
 
-2. [WBZ451_MICRF220_219A](https://github.com/MicrochipTech/PIC32CXBZ2_WBZ45x_Sub-GHz_MICRF112_114_MICRF220_219A_BLE_SENSOR/tree/main/WBZ451_MICRF220_219A)
+2. [WBZ451_MICRF220/219A](https://github.com/MicrochipTech/PIC32CXBZ2_WBZ45x_Sub-GHz_MICRF112_114_MICRF220_219A_BLE_SENSOR/tree/main/WBZ451_MICRF220_219A)
 
---
+++ WBZ451_MICRF220_219A/micrf220_219a/dvr_crc.c
@@ -0,0 +1,122 @@
+// <editor-fold defaultstate="collapsed" desc="File Header">
+/***********************************************************************************************************************
+ *
+ * Filename:   dvr_crc.c
+ *
+ * Global Designator: DVR_CRC_
+ *
+ * Contents: Interfaces the hardware CRC module created by MCC.
+ * 
+ ***********************************************************************************************************************
+ * © 2023 Microchip Technology Inc. and its subsidiaries.  You may use this software and any derivatives exclusively
+ * with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS
+ * SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A PARTICULAR
+ * PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE,
+ * COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF
+ * THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON
+ * ALL CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY, THAT YOU HAVE PAID
+ * DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ * 
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE TERMS. 
+ **********************************************************************************************************************/
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="Include Files">
+/* ****************************************************************************************************************** */
+/* INCLUDE FILES */
+
+#include "dvr_crc.h"
+#include <stdio.h>
+#include <stdint.h>
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="Macro Definitions">
+/* ****************************************************************************************************************** */
+/* MACRO DEFINITIONS */
+#define POLY 0x8005
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="Type Definitions">
+/* ****************************************************************************************************************** */
+/* TYPE DEFINITIONS */
+
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="Local Function Prototypes">
+/* ****************************************************************************************************************** */
+/* FUNCTION PROTOTYPES */
+
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="Constant Definitions">
+/* ****************************************************************************************************************** */
+/* CONSTANTS */
+
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="File Variables - Static">
+/* ****************************************************************************************************************** */
+/* FILE VARIABLE DEFINITIONS */
+
+// </editor-fold>
+
+/* ****************************************************************************************************************** */
+/* FUNCTION DEFINITIONS */
+
+// <editor-fold defaultstate="collapsed" desc="uint16_t DVR_CRC_calc( void *pData, uint8_t cnt )">
+
+/***********************************************************************************************************************
+ *
+ * Function Name: DVR_CRC_calc
+ *
+ * Purpose: Calculates the CRC on the data past in, pData.
+ *
+ * Arguments: void *pData, uint8_t cnt
+ *
+ * Returns: uint16_t - CRC Result
+ *
+ * Side Effects: Time!
+ *
+ * Reentrant Code: No
+ *
+ * Notes:  This is blocking code.  The function will return after the CRC is calculated.
+ * 
+ **********************************************************************************************************************/
+
+uint16_t crc16(void *pData, uint8_t cnt) 
+{
+    uint8_t *data = (uint8_t *)pData;
+    uint16_t crc = 0xFFFF;
+    for (int i = 0; i < cnt; i++) 
+    {
+        crc ^= (uint16_t)data[i] << 8;
+        for (int j = 0; j < 8; j++) 
+        {
+            if (crc & 0x8000) 
+            {
+                crc = (crc << 1) ^ POLY;
+            } 
+            else 
+            {
+                crc <<= 1;
+            }
+        }
+    }
+ return crc;
+}
+/* ****************************************************************************************************************** */
+// </editor-fold>
+
+/* ****************************************************************************************************************** */
+/* Local Functions */
+
+/* ****************************************************************************************************************** */
+/* Event Handlers */
+
+/* ****************************************************************************************************************** */
+/* Unit Test Code */
+
+
